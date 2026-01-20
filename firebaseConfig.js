b91f8ba40a649f71fd225071c33f0de43e6a1921
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2026-01-20 05:10:46 +0200

    Update Firebase configuration with real credentials

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,18 +1,23 @@
+// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
+// بيانات مشروعك الحقيقية التي أرسلتها
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
+  apiKey: "AIzaSyAmxDBecXP2bikBqI9PANfcEkCq2s4DXDg",
   authDomain: "alwarsha-cf816.firebaseapp.com",
-  databaseURL: "https://alwarsha-cf816-default-rtdb.firebaseio.com/",
+  databaseURL: "https://alwarsha-cf816-default-rtdb.firebaseio.com",
   projectId: "alwarsha-cf816",
-  storageBucket: "alwarsha-cf816.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  storageBucket: "alwarsha-cf816.firebasestorage.app",
+  messagingSenderId: "787131606543",
+  appId: "1:787131606543:web:276d6bac72cbd3fa40257a",
+  measurementId: "G-1SHMBCMTG2"
 };
 
-// التأكد من عدم تهيئة التطبيق أكثر من مرة
+// تهيئة التطبيق
 const app = initializeApp(firebaseConfig);
+
+// تصدير الأدوات لباقي الموقع (مهم جداً لعمل الصفحات)
 export const auth = getAuth(app);
 export const db = getDatabase(app);
